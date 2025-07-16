fe361e9a1d5b6a5e73a4a4692925c7e444a67799
yocotera14 <yosefcotmar@gmail.com>
2025-07-16 16:49:30 -0500

    Add files via upload

---
+++ main/panelProducto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author josef
+ */
+class panelProducto {
+
+    static void add(JButton btnEliminarProducto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
